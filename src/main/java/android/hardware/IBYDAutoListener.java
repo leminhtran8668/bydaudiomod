23b96051d9393736d28c2755e05b48fbaea67372
Lê Minh <leminh@bydaudiomod.local>
2026-08-12 04:40:56 +0000

    Fix DEX merge: compile android.hardware stubs only, do not package into app (avoid duplicate with byd_sdk.dex)

--- src/main/java/android/hardware/IBYDAutoListener.java
+++
@@ -1,9 +0,0 @@
-package android.hardware;
-
-import android.hardware.bydauto.BYDAutoEventValue;
-
-public interface IBYDAutoListener {
-    void onDataChanged(IBYDAutoEvent event);
-    void onDataEventChanged(int eventType, BYDAutoEventValue eventValue);
-    void onError(int errCode, String errMessage);
-}
--- stubs/android/hardware/IBYDAutoListener.java
+++ stubs/android/hardware/IBYDAutoListener.java
@@ -1,4 +1,9 @@
 package android.hardware;
 
+import android.hardware.bydauto.BYDAutoEventValue;
+
 public interface IBYDAutoListener {
+    void onDataChanged(IBYDAutoEvent event);
+    void onDataEventChanged(int eventType, BYDAutoEventValue eventValue);
+    void onError(int errCode, String errMessage);
 }
